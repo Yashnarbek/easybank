ad661386b6f5af4ed2b7965b54b0fd8821ea1064
Yashnarbek <ymqayumov@gmail.com>
2022-08-15 18:42:42 +0300

    Add responsive

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
-let elCreatePlane = document.querySelector(".js-create-plane")
-let elModalBox = document.querySelector(".modal-box")
-let elBody = document.querySelector("body")
+let burger = document.querySelector(".site-header__burger")
+let nav = document.querySelector(".header-inner__nav")
 
-elCreatePlane.addEventListener("click", function(){
-    elModalBox.classList.add("modal-close")
-    elBody.classList.add("hidden")
+burger.addEventListener("click", function(){
+    nav.classList.toggle("active")
+    burger.classList.toggle("close")
+
 })
-elModalBox.addEventListener("click", function(evt){
-    evt.preventDefault()
-    elModalBox.classList.remove("modal-close")
-    elBody.classList.remove("hidden")
-})
-console.log(elCreatePlane)
+
